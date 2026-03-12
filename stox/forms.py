8e6049ab500b50c8c1dcad0bc4382e24cebfb60b
TimurT <ofset989@gmai.com>
2026-03-12 18:28:50 +0100

    final_commit

--- stox/forms.py
+++ stox/forms.py
@@ -4,6 +4,16 @@ class PriceCheckForm(forms.Form):
     url = forms.URLField()
     size = forms.CharField()
 
+    def stockxurl(self):
+        if "stockx" not in (self.url):
+            raise forms.ValidationError("The url must be from the StockX site.")
+        
+        return self.url
+    
+    def size_validation(self):
+        if self.size > 52.5 or self.size < 35.5:
+            raise forms.ValidationError("There no such size in the StockX site. Try one smaller or bigger.")
+
 class LoginForm(forms.Form):
     username = forms.CharField(max_length=100)
     password = forms.CharField(widget=forms.PasswordInput)
